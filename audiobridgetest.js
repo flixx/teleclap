d379336e2e1dff5fb5d78f4c4110a63b7af00b1e
jkhlr <13289385+jkhlr@users.noreply.github.com>
2020-03-22 15:05:50 +0100

    Update backend URL

--- audiobridgetest.js
+++ audiobridgetest.js
@@ -42,11 +42,7 @@
 // in the presented order. The first working server will be used for
 // the whole session.
 //
-var server = null;
-if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
-else
-	server = "https://" + window.location.hostname + ":8089/janus";
+var server = "http://18.194.48.243:8088/janus";
 
 var janus = null;
 var mixertest = null;
