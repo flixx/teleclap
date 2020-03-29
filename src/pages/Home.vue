01da65a62cb0755be16d9ab05126c01b9ce72328
Florian Strasser <56030947+flostrasser@users.noreply.github.com>
2020-03-29 23:07:36 +0200

    Updates

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,36 +1,43 @@
 <template>
   <main-layout>
-    <h1>Open a new Room</h1>
-    <input type=text  autocomplete="off" v-model="roomName" data-lpignore="true"/>
-    <button v-on:click="openRoom" id="openRoom">Open</button>
+    <section>
+      <div class="container">
+        <button class="btn btn-primary" autocomplete="off" id="record">TeleClap</button>
+        <button class="btn btn-primary" autocomplete="off" id="listen">Listen to all</button>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <h1>Open a new Room</h1>
+        <input type="text" autocomplete="off" v-model="roomName" data-lpignore="true" />
+        <button v-on:click="openRoom" id="openRoom">Open</button>
+      </div>
+    </section>
   </main-layout>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
-  import routes from '../routes'
-  import utils from '../utils'
+import MainLayout from "../layouts/Main.vue";
+import routes from "../routes";
+import utils from "../utils";
 
-  export default {
-    components: {
-      MainLayout
-    },
-    data: function() {
-      return {
-        roomName: ''
-      }
-    },
-    methods: {
-      openRoom: function (event) {
-        event.preventDefault()
-        var slug = utils.slugify(this.roomName)
-        this.$root.currentRoute = slug
-        window.history.pushState(
-          null,
-          slug,
-          slug
-        )
-      }
+export default {
+  components: {
+    MainLayout
+  },
+  data: function() {
+    return {
+      roomName: ""
+    };
+  },
+  methods: {
+    openRoom: function(event) {
+      event.preventDefault();
+      var slug = utils.slugify(this.roomName);
+      this.$root.currentRoute = slug;
+      window.history.pushState(null, slug, slug);
     }
   }
+};
 </script>
--- src/pages/Room.vue
+++ src/pages/Room.vue
@@ -1,21 +1,25 @@
 <template>
   <main-layout>
-    <h1>Your room: </h1>
-    <input type="text" readonly v-bind:value="myPath" />
+    <section>
+      <div class="container">
+        <h1>Your room:</h1>
+        <input type="text" readonly v-bind:value="myPath" />
+      </div>
+    </section>
   </main-layout>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
+import MainLayout from "../layouts/Main.vue";
 
-  export default {
-    components: {
-      MainLayout
-    },
-    computed: {
-      myPath: function () {
-        return window.location.href
-      }
+export default {
+  components: {
+    MainLayout
+  },
+  computed: {
+    myPath: function() {
+      return window.location.href;
     }
   }
+};
 </script>
