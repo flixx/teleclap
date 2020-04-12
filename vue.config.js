038393f0752fca2e780d7d419f8cc689ababe466
Felix <fb@3yourmind.com>
2020-04-12 23:12:40 +0200

    Finish up move to vue cli

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+module.exports = {
+    configureWebpack: {
+      plugins: [
+        // See https://janus.conf.meetecho.com/docs/js-modules.html
+        new webpack.ProvidePlugin({ adapter: 'webrtc-adapter' })
+      ],
+      module: {
+        rules: [
+          // See https://janus.conf.meetecho.com/docs/js-modules.html
+          {
+            test: require.resolve('janus-gateway'),
+            use: 'exports-loader?Janus=Janus'
+          }
+        ]
+      }
+    }
+  }
